a0041c523da10a7dd43aee5186fd9afd706f80e4
Cristian Prigoana <cristian.prigoana@yahoo.com>
2016-03-30 13:05:30 +0200

    Compute time distribution

---
+++ src/GraphStatistics.java
@@ -0,0 +1,68 @@
+import org.neo4j.graphdb.*;
+import util.histogram.GraphTimeDistribution;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by cristiprg on 30-3-16.
+ */
+public class GraphStatistics {
+
+    /**
+     * Computes the time distribution of friendships timestamps by: hour, day, month;
+     * And then, it stores this as JSON property of STATS node.
+     * @param graphDB
+     */
+    public static void insertTimeDistribution(GraphDatabaseService graphDB) {
+
+        GraphTimeDistribution distribution = new GraphTimeDistribution();
+        try(Transaction tx = graphDB.beginTx()) {
+
+            // 1. compute histograms
+            Label label = DynamicLabel.label("Friendship");
+            try (ResourceIterator<Node> friendships = graphDB.findNodes(label)) {
+
+                while (friendships.hasNext()) {
+                    // read the timestamp from the friendship node
+                    String stringDate = (String)friendships.next().getProperty("timestamp");
+
+                    // discard non-existing data
+                    if (stringDate.equals("0")) continue;
+
+                    // convert timestamp to calendar type
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(new Date(Long.valueOf(stringDate) * 1000L));
+
+                    // add stuff to the histograms
+                    distribution.addHourPoint(cal.get(Calendar.HOUR_OF_DAY));
+                    distribution.addDayPoint(cal.get(Calendar.DAY_OF_YEAR));
+                    distribution.addMonthPoint(cal.get(Calendar.MONTH));
+                }
+            }
+
+            // test for now what we've done so far
+            try {
+
+                FileWriter file = new FileWriter("test.json");
+                file.write(distribution.getJSON().toJSONString());
+                file.flush();
+                file.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // 2. insert STATS node
+//            Node statsNode = graphDB.createNode();
+//            statsNode.addLabel(DynamicLabel.label("facebookStats"));
+//            statsNode.setProperty("stats", distribution.toJSON().toString());
+
+            tx.success();
+        }
+
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,19 @@
-import org.apache.flink.graph.Vertex;
-import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class Main {
 
     public static void main(String[] args) {
-        Vertex<Long, String> v = new Vertex<>(1L, "foo");
-       // Vertex<Long, NullValue> v2 = new Vertex<>();
 
-        System.out.println(v.getValue());
-
-        // keep in mind that the neo4j-service has to be stopped before running this thing.
-        GraphDatabaseService graphDB = new GraphDatabaseFactory()
-                .newEmbeddedDatabase("/home/cristiprg/neo4jDB");
-
-        try(Transaction tx = graphDB.beginTx()) {
-
-            Label label = DynamicLabel.label("Friendship");
-            try (ResourceIterator<Node> friendships =
-                         graphDB.findNodes(label)) {
-
-                while (friendships.hasNext()) {
-                    System.out.println(friendships.next().getProperty("timestamp"));
-                }
-            }
-
-            tx.success();
+        GraphDatabaseService graphDB = null;
+        try{
+            // keep in mind that the neo4j-service has to be stopped before running this thing.
+            graphDB = new GraphDatabaseFactory().newEmbeddedDatabase("/home/cristiprg/neo4jDB");
+            GraphStatistics.insertTimeDistribution(graphDB);
+        }finally {
+            // without explicit shutdown, the service doesn't start anymore and shows no error (at least on Ubuntu)
+            if (graphDB != null)
+                graphDB.shutdown();
         }
-
-        // without explicit shutdown, the service doesn't start anymore and shows no error (at least on Ubuntu)
-        graphDB.shutdown();
     }
 }
