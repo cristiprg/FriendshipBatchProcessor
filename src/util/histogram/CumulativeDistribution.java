9369dc86e2a161366187f0bec301828371342881
Cristian Prigoana <cristian.prigoana@yahoo.com>
2016-03-31 00:27:53 +0200

    Add the cumulative dist class

---
+++ src/util/histogram/CumulativeDistribution.java
@@ -0,0 +1,66 @@
+package util.histogram;
+
+import  org.json.simple.JSONArray;
+import  org.json.simple.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by cristiprg on 30-3-16.
+ */
+public class CumulativeDistribution {
+    private long minTimestamp = Long.MAX_VALUE;
+    private long maxTimestamp = Long.MIN_VALUE;
+    private long nrSteps = 0;
+    private long stepSize = 0;
+    private int[] buckets;
+
+    public CumulativeDistribution(long minTimestamp, long maxTimestamp, int nrSteps) {
+        this.minTimestamp = minTimestamp;
+        this.maxTimestamp = maxTimestamp;
+        this.nrSteps = nrSteps;
+        this.stepSize = (maxTimestamp - minTimestamp) / nrSteps;
+        buckets = new int[nrSteps];
+    }
+
+    public void addTimestamp(long timestamp){
+        int index =(int) ((timestamp - minTimestamp) / stepSize);
+        if (index == buckets.length) --index;
+        ++buckets[index];
+    }
+
+    public JSONObject getJSON(){
+        JSONObject obj = new JSONObject();
+        obj.put("values", getBucketValuesJSONArray());
+        obj.put("ticks", getBucketIndexesJSONArray());
+        return obj;
+    }
+
+    private JSONArray getBucketValuesJSONArray(){
+        JSONArray list = new JSONArray();
+        for(int data : buckets)
+            list.add(data);
+
+        return list;
+    }
+
+    /**
+     * The indexes returned are in fact dates in format 'YYYY-MM-DD'... as opposed to integers
+     */
+    private JSONArray getBucketIndexesJSONArray(){
+        JSONArray list = new JSONArray();
+        for (int i = 0; i < buckets.length; ++i){
+            long timeStamp = minTimestamp + i * stepSize;
+            Date time = new Date(timeStamp);
+            DateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+            String stringTime = df.format(time);
+            list.add(stringTime);
+        }
+        return list;
+    }
+}
